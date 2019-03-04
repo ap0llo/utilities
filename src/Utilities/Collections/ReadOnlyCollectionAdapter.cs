1a53179e300d05ccbc58dfbc2f4445b7a8d661dc
Andreas Gruenwald <andreasgruenwald@live.de>
2019-03-04 21:30:04 +0100

    Add ReadOnlyCollectionAdapter and ReadOnlyDictionary

---
+++ src/Utilities/Collections/ReadOnlyCollectionAdapter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Grynwald.Utilities.Collections
+{
+    public static class ReadOnlyCollectionAdapter
+    {
+        /// <summary>
+        /// Creates a new read-only wrapper from the specified collection
+        /// </summary>
+        /// <typeparam name="T">The collection's element type.</typeparam>
+        /// <param name="collection">The collection to wrap in a read-only adapter</param>
+        /// <returns>Returns a new instance of <see cref="ReadOnlyCollectionAdapter{T}"/> wrapping the specified instance</returns>
+        public static ReadOnlyCollectionAdapter<T> Create<T>(ICollection<T> collection) =>
+            new ReadOnlyCollectionAdapter<T>(collection);
+    }
+
+    /// <summary>
+    /// Wraps an instance of <see cref="ICollection{T}"/> as an <see cref="IReadOnlyCollection{T}"/>
+    /// </summary>
+    public class ReadOnlyCollectionAdapter<T> : IReadOnlyCollection<T>
+    {
+        private readonly ICollection<T> m_Collection;
+
+
+        public int Count => m_Collection.Count;
+
+
+        public ReadOnlyCollectionAdapter(ICollection<T> collection)
+        {
+            m_Collection = collection ?? throw new ArgumentNullException(nameof(collection));
+        }
+
+
+        public IEnumerator<T> GetEnumerator() => m_Collection.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => m_Collection.GetEnumerator();
+    }
+}
---
+++ src/Utilities/Collections/ReadOnlyDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Grynwald.Utilities.Collections
+{
+    /// <summary>
+    /// Singleton implementation of <see cref="IReadOnlyDictionary{TKey, TValue}"/> .
+    /// </summary>
+    /// <remarks>
+    /// <see cref="ReadOnlyDictionary{TKey, TValue}"/> provides a empty singleton dictionary
+    /// analogous to <see cref="Array.Empty{T}"/>
+    /// </remarks>
+    /// <example><![CDATA[
+    /// public void Method1(IReadOnlyDictionary<string, string> dictionary = null)
+    /// {
+    ///     dictionary = dictionary ?? ReadOnlyDictionar<string, string>.Empty;
+    /// }
+    /// ]]>
+    /// </example>
+    /// <typeparam name="TKey">The type of keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
+    public class ReadOnlyDictionary<TKey, TValue> : IReadOnlyDictionary<TKey, TValue>
+    {
+        /// <summary>
+        /// The singleton instance of <see cref="ReadOnlyDictionary{TKey, TValue}"/>
+        /// </summary>
+        public static IReadOnlyDictionary<TKey, TValue> Empty = new ReadOnlyDictionary<TKey, TValue>();
+
+
+        public TValue this[TKey key] => throw new KeyNotFoundException();
+
+        public IEnumerable<TKey> Keys => Enumerable.Empty<TKey>();
+
+        public IEnumerable<TValue> Values => Enumerable.Empty<TValue>();
+
+        public int Count => 0;
+
+        public bool ContainsKey(TKey key) => false;
+
+        private ReadOnlyDictionary()
+        { }
+
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => Enumerable.Empty<KeyValuePair<TKey, TValue>>().GetEnumerator();
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            value = default;
+            return false;            
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => Enumerable.Empty<KeyValuePair<TKey, TValue>>().GetEnumerator();
+    }
+}
