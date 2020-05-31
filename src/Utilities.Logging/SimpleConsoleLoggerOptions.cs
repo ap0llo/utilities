0510224190b4bd2faf6b61bcecfdd5ab02d67ca2
Andreas Gruenwald <andreas@grynwald.net>
2020-05-31 19:34:24 +0200

    WIP: Add Logger implementation
    Add SimpleConsoleLogger and SimpleConsoleLoggerOptions

---
+++ src/Utilities.Logging/SimpleConsoleLoggerOptions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Grynwald.Utilities.Logging
+{
+    /// <summary>
+    /// Settings for <see cref="SimpleConsoleLogger"/>
+    /// </summary>
+    public sealed class SimpleConsoleLoggerOptions
+    {
+        /// <summary>
+        /// Gets the minimum log level of log messages that are written to the output
+        /// </summary>
+        public LogLevel MinimumLogLevel { get; }
+
+        /// <summary>
+        /// Gets whether the category name of log messages is included in the output
+        /// </summary>
+        public bool ShowCategoryName { get; }
+
+        /// <summary>
+        /// Gets whether console output uses colors to differentiate between log levels
+        /// </summary>
+        public bool EnableColoredOutput { get; }
+
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="SimpleConsoleLoggerOptions"/>
+        /// </summary>
+        /// <param name="minimumLogLevel">The value to use for <see cref="MinimumLogLevel"/></param>
+        /// <param name="showCategoryName">The value to use for <see cref="ShowCategoryName"/></param>
+        /// <param name="enabledColoredOutput">The value to use for <see cref="EnableColoredOutput"/></param>
+        public SimpleConsoleLoggerOptions(LogLevel minimumLogLevel = LogLevel.Information, bool showCategoryName = true, bool enabledColoredOutput = true)
+        {
+            MinimumLogLevel = minimumLogLevel;
+            ShowCategoryName = showCategoryName;
+            EnableColoredOutput = enabledColoredOutput;
+        }
+    }
+}
