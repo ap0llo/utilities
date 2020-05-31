118e4473cc622b705de9eb1ebd474c4763c0207a
Andreas Gruenwald <andreas@grynwald.net>
2020-05-31 19:47:01 +0200

    WIP: Add Logger implementation
    Add ILoggerProvider implemeantation

--- src/Utilities.Logging/SimpleConsoleLoggerOptions.cs
+++ src/Utilities.Logging/SimpleConsoleLoggerOptions.cs
@@ -8,6 +8,11 @@ namespace Grynwald.Utilities.Logging
     public sealed class SimpleConsoleLoggerOptions
     {
         /// <summary>
+        /// Gets the default logger settings
+        /// </summary>
+        public static readonly SimpleConsoleLoggerOptions Default = new SimpleConsoleLoggerOptions(LogLevel.Information, true, true);
+
+        /// <summary>
         /// Gets the minimum log level of log messages that are written to the output
         /// </summary>
         public LogLevel MinimumLogLevel { get; }
@@ -29,7 +34,7 @@ namespace Grynwald.Utilities.Logging
         /// <param name="minimumLogLevel">The value to use for <see cref="MinimumLogLevel"/></param>
         /// <param name="showCategoryName">The value to use for <see cref="ShowCategoryName"/></param>
         /// <param name="enabledColoredOutput">The value to use for <see cref="EnableColoredOutput"/></param>
-        public SimpleConsoleLoggerOptions(LogLevel minimumLogLevel = LogLevel.Information, bool showCategoryName = true, bool enabledColoredOutput = true)
+        public SimpleConsoleLoggerOptions(LogLevel minimumLogLevel, bool showCategoryName, bool enabledColoredOutput)
         {
             MinimumLogLevel = minimumLogLevel;
             ShowCategoryName = showCategoryName;
---
+++ src/Utilities.Logging/SimpleConsoleLoggerProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+namespace Grynwald.Utilities.Logging
+{
+    internal sealed class SimpleConsoleLoggerProvider : ILoggerProvider
+    {
+        private static readonly object s_Lock = new object();
+        private readonly IDictionary<string, ILogger> m_Loggers = new Dictionary<string, ILogger>();
+        private readonly SimpleConsoleLoggerOptions m_LoggerOptions;
+
+        public SimpleConsoleLoggerProvider() : this(SimpleConsoleLoggerOptions.Default)
+        { }
+
+        public SimpleConsoleLoggerProvider(SimpleConsoleLoggerOptions loggerOptions)
+        {
+            m_LoggerOptions = loggerOptions ?? throw new ArgumentNullException(nameof(loggerOptions));
+        }
+
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            lock (s_Lock)
+            {
+                if (m_Loggers.TryGetValue(categoryName, out var logger))
+                {
+                    return logger;
+                }
+                else
+                {
+                    logger = new SimpleConsoleLogger(m_LoggerOptions, categoryName);
+                    m_Loggers.Add(categoryName, logger);
+                    return logger;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            lock (s_Lock)
+            {
+                m_Loggers.Clear();
+            }
+        }
+    }
+}
